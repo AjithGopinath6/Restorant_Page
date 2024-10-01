7b405170e1850aea39e5c2860d35268718bf8497
Ajith Gopinath <ajithgopinath639@gmail.com>
2024-10-01 10:41:07 +0530

    created about coomponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import './App.css'
 import Home from './Components/Home'
+import About from './Components/About'
+
 
 
 function App() {
   return (
     <div className='App'>
       <Home/>
+      <About />
     </div>
   )
 }
